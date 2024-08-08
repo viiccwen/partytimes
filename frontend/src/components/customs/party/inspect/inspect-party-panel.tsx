5a6086623afc29a73fff033ed2da550df4d448dc
viiccwen <vic20050418@gmail.com>
2024-08-08 20:11:46 +0800

    feat: create vote feature: * show vote block * add cascade with vote and timeslot * change time from int to float fix: * delete party with foreign key todo: * refetch and rerender blocks after voting * change appearance after editing

--- frontend/src/components/customs/party/inspect/inspect-party-panel.tsx
+++ frontend/src/components/customs/party/inspect/inspect-party-panel.tsx
@@ -1,14 +1,28 @@
-import { party_return_schema_type } from "@/lib/type";
+"use server";
+
+import { party_return_schema_type, votes_schema_type } from "@/lib/type";
 import { PartyJoinCard } from "./party-join-card";
 import { PartyTimelineCard } from "./party-timeline-card";
 import { CreateVote, GetVoteTimes } from "@/actions/vote-actions";
+import { ampm } from "@/lib/schema";
+import { redirect } from "next/navigation";
 
 interface InspectPartyPanelProps {
   party: party_return_schema_type;
 }
 
+type block_type = {
+  creatorName: string;
+};
+
 export const InspectPartyPanel = async ({ party }: InspectPartyPanelProps) => {
   const votes = await GetVoteTimes(party.partyid);
+  const total_hours = CalculateTotalHours(party);
+  let voteBlocks: Array<block_type[]>[] = CreateTimeSlotBlocks(
+    votes.data,
+    total_hours,
+    party
+  );
 
   return (
     <>
@@ -17,11 +31,57 @@ export const InspectPartyPanel = async ({ party }: InspectPartyPanelProps) => {
           <PartyTimelineCard
             className="col-span-4"
             party={party}
-            votes={votes.data}
+            votes={voteBlocks}
           />
           <PartyJoinCard className="col-span-2" joinList={null} />
         </div>
       </div>
     </>
   );
+};
+
+const CalculateTotalHours = (party: party_return_schema_type): number => {
+  let start_time = party.start_time === 12 ? 0 : party.start_time;
+  start_time =
+    party.start_ampm === ampm[0] ? party.start_time : party.start_time + 12;
+
+  let end_time = party.end_time === 12 ? 0 : party.end_time;
+  end_time = party.end_ampm === ampm[0] ? party.end_time : party.end_time + 12;
+
+  return end_time - start_time;
+};
+
+const CreateTimeSlotBlocks = (
+  votes: votes_schema_type[],
+  total_hours: number,
+  party: party_return_schema_type
+) => {
+  let blocks: Array<block_type[]>[] = Array.from(
+    { length: party.date.length },
+    () => Array.from({ length: total_hours * 2 }, () => [])
+  );
+
+  votes.forEach((vote: votes_schema_type) => {
+    vote.timeslots.forEach((timeslot) => {
+      const date = timeslot.date;
+      const row = party.date.findIndex((v) => v === date);
+
+      const start_time = timeslot.start_time;
+      const end_time = timeslot.end_time;
+      const start_ampm = timeslot.start_ampm;
+      const end_ampm = timeslot.end_ampm;
+
+      const start =
+        (start_time === 12 ? 0 : start_time) + (start_ampm === "PM" ? 12 : 0) - party.start_time;
+      const end = (end_time === 12 ? 0 : end_time) + (end_ampm === "PM" ? 12 : 0) - party.start_time;
+      // console.log(start, end);
+      // push start to end blocks
+      for (let i = start; i < end; i += 0.5) {
+        const col = i * 2;
+        blocks[row][col].push({ creatorName: vote.creatorName });
+      }
+    });
+  });
+
+  return blocks;
 };
--- frontend/src/components/customs/party/inspect/party-timeline-card.tsx
+++ frontend/src/components/customs/party/inspect/party-timeline-card.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Card, CardContent } from "@/components/ui/card";
-import { party_return_schema_type, timeslots_create_schema_type, votes_schema_type } from "@/lib/type";
 import {
   ReactElement,
   ReactNode,
@@ -8,18 +7,147 @@ import {
   useEffect,
   useState,
 } from "react";
-import { PartyTimelineHeader } from "./party-timeline-header";
 import { Separator } from "@/components/ui/separator";
 import { toast } from "sonner";
 import { PartyHeader } from "./party-header";
+import { PartyTimelineHeader } from "./party-timeline-header";
+
 import { ampm } from "@/lib/schema";
+import {
+  party_return_schema_type,
+  timeslots_create_schema_type,
+} from "@/lib/type";
 import { CreateVote } from "@/actions/vote-actions";
 
 interface PartyTimelineCardProps {
   className?: string;
   party: party_return_schema_type;
-  votes: votes_schema_type;
+  votes: Array<block_type[]>[];
 }
+
+type block_type = {
+  creatorName: string;
+};
+
+const Cookie = require("js-cookie");
+
+export const PartyTimelineCard = ({
+  className,
+  party,
+  votes,
+}: PartyTimelineCardProps) => {
+  const [timeLineComponent, setTimeLineComponent] = useState<ReactElement>();
+  const [selectBlock, setSelectBlock] = useState<Set<string>>(new Set());
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [hydrated, setHydrated] = useState<boolean>(false);
+  const [Blocks, setBlocks] = useState<Array<block_type[]>[]>(votes);
+
+  const HandleTimeBlock = useCallback(
+    (row: number, col: number, isDragging: boolean) => {
+      if (!isEditing) {
+        toast.error("請先進入編輯模式");
+        return;
+      }
+
+      const block_key = `${col}-${row}`;
+      setSelectBlock((prev) => {
+        if (prev.has(block_key)) prev.delete(block_key);
+        else prev.add(block_key);
+
+        return new Set(prev);
+      });
+    },
+    [isEditing]
+  );
+
+  useEffect(() => {
+    const total_hours = CalculateTotalHours(party);
+    const total_half_hours = total_hours * 2;
+    const header = generateHeader(party);
+    const gridCells = generateGridCells(
+      party,
+      total_half_hours,
+      HandleTimeBlock,
+      selectBlock,
+      isEditing,
+      Blocks
+    );
+
+    const container = (
+      <div>
+        {header}
+        {gridCells}
+      </div>
+    );
+
+    setTimeLineComponent(container);
+  }, [party, HandleTimeBlock, selectBlock]);
+
+  const HandleCheckButton = async () => {
+    if (!isEditing) {
+      setIsEditing(true);
+      toast.success("已進入編輯模式");
+    } else if (selectBlock.size === 0) {
+      toast.error("請選擇時間區塊！");
+    } else {
+      const timeslots = GenerateTimeSlots(selectBlock, party);
+
+      const func = async () => {
+        const res = await CreateVote(
+          timeslots,
+          party.partyid,
+        );
+
+        if (!res.correct) toast.error(res.error);
+        else {
+          toast.success("已成功送出投票");
+          window.location.reload();
+        }
+      };
+
+      func();
+    }
+  };
+
+  // todo: implement
+  const HandleScheduleButton = () => {
+    console.log("Schedule Button Click");
+  };
+
+  useEffect(() => {
+    setHydrated(true);
+
+    const handleMouseUp = () => setIsDragging(false);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => window.removeEventListener("mouseup", handleMouseUp);
+  }, []);
+
+  if (!hydrated) {
+    return null;
+  } else {
+    return (
+      <Card className={className}>
+        <CardContent>
+          <PartyHeader className="mt-5" party={party} />
+          <Separator className="h-1 my-3" />
+          <PartyTimelineHeader
+            className="mt-5"
+            party={party}
+            HandleCheckButton={HandleCheckButton}
+            HandleScheduleButton={HandleScheduleButton}
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+            setSelectBlock={setSelectBlock}
+          />
+          {timeLineComponent}
+          {JSON.stringify(votes, null, 2)}
+        </CardContent>
+      </Card>
+    );
+  }
+};
 
 const CalculateTotalHours = (party: party_return_schema_type): number => {
   let start_time = party.start_time === 12 ? 0 : party.start_time;
@@ -45,7 +173,8 @@ const generateGridCells = (
   total_half_hours: number,
   HandleTimeBlock: (row: number, col: number, isDragging: boolean) => void,
   selectBlock: Set<string>,
-  isEditing: boolean
+  isEditing: boolean,
+  Blocks: block_type[][][]
 ): ReactNode[] => {
   let components: ReactNode[] = [];
   const date_length = party.date.length;
@@ -62,11 +191,17 @@ const generateGridCells = (
           className={`${
             row % 2 == 0
               ? "border-t-2"
-              : (row == total_half_hours - 1
+              : row == total_half_hours - 1
               ? "border-b-2"
-              : "")
+              : ""
           } border-x border-slate-500 h-[24px] col-auto hover:border-2 hover:border-dashed hover:cursor-row-resize select-none
-          ${isSelected ? "bg-blue-400" : isEditing ? "bg-blue-300" : ""}
+          ${
+            isSelected
+              ? "bg-blue-400"
+              : Blocks[col]?.[row]?.length > 0
+              ? "bg-green-400"
+              : ""
+          }
           `}
           onMouseDown={() => HandleTimeBlock(row, col, false)}
           onMouseEnter={(e) => {
@@ -83,14 +218,15 @@ const generateGridCells = (
         ? formatTime(party.start_time + hour, party.start_ampm)
         : "";
 
+    // bug: sometimes the grid is not working properly
+    const grid_cols = `grid-cols-${date_length.toString()}`;
+
     components.push(
       <div key={row} className="flex">
         <div className="flex justify-end items-center w-[60px] text-slate-600 select-none">
           {time}
         </div>
-        <div className={`grid grid-cols-${date_length} w-full ml-2`}>
-          {rowCells}
-        </div>
+        <div className={`grid ${grid_cols} w-full ml-2`}>{rowCells}</div>
       </div>
     );
   }
@@ -137,8 +273,6 @@ const GenerateTimeSlots = (
 
   // merge continuous blocks into one
 
-  
-
   let prev = { row: -1, col: -1 };
   let start_time: number = -1;
   let start_ampm: string = "";
@@ -159,6 +293,10 @@ const GenerateTimeSlots = (
       end_ampm = GetTimeWithAMPM(end_time, party.start_ampm);
       prev = cur;
     } else {
+      // change to 12 hours format
+      if (start_time >= 12) start_time -= 12;
+      if (end_time >= 12) end_time -= 12;
+
       timeSlots.push({
         date: party.date[prev.row],
         start_time: start_time,
@@ -177,6 +315,10 @@ const GenerateTimeSlots = (
 
   // check for last one time slot
   if (start_time !== -1 && end_time !== -1) {
+    // change to 12 hours format
+    if (start_time >= 12) start_time -= 12;
+    if (end_time >= 12) end_time -= 12;
+
     timeSlots.push({
       date: party.date[raw_timeSlots[raw_timeSlots.length - 1].row],
       start_time: start_time,
@@ -187,119 +329,4 @@ const GenerateTimeSlots = (
   }
 
   return timeSlots;
-};
-
-export const PartyTimelineCard = ({
-  className,
-  party,
-  votes,
-}: PartyTimelineCardProps) => {
-  const [timeLineComponent, setTimeLineComponent] = useState<ReactElement>();
-  const [selectBlock, setSelectBlock] = useState<Set<string>>(new Set());
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [hydrated, setHydrated] = useState<boolean>(false);
-
-  const HandleTimeBlock = useCallback(
-    (row: number, col: number, isDragging: boolean) => {
-      if (!isEditing) {
-        toast.error("請先進入編輯模式");
-        return;
-      }
-
-      const block_key = `${col}-${row}`;
-      setSelectBlock((prev) => {
-        if (prev.has(block_key)) prev.delete(block_key);
-        else prev.add(block_key);
-
-        return new Set(prev);
-      });
-    },
-    [isEditing]
-  );
-
-  useEffect(() => {
-    const total_hours = CalculateTotalHours(party);
-    const total_half_hours = total_hours * 2;
-    const header = generateHeader(party);
-    const gridCells = generateGridCells(
-      party,
-      total_half_hours,
-      HandleTimeBlock,
-      selectBlock,
-      isEditing
-    );
-
-    const container = (
-      <div className={`grid grid-rows-${total_half_hours}`}>
-        {header}
-        {gridCells}
-      </div>
-    );
-
-    setTimeLineComponent(container);
-  }, [party, HandleTimeBlock, selectBlock]);
-
-  const HandleCheckButton = async () => {
-    if (!isEditing) {
-      setIsEditing(true);
-      toast.success("已進入編輯模式");
-    } else if (selectBlock.size === 0) {
-      toast.error("請選擇時間區塊！");
-    } else {
-      const timeslots = GenerateTimeSlots(selectBlock, party);
-      const response = await CreateVote(timeslots, party.partyid);
-
-      if(response.correct) {
-        setIsEditing(false);
-        setSelectBlock(new Set());
-        toast.success("已成功送出投票");
-      } else {
-        toast.error(response.error);
-      }
-    }
-  };
-
-  // todo: implement
-  const HandleScheduleButton = () => {
-    console.log("Schedule Button Click");
-  };
-
-  useEffect(() => {
-    setHydrated(true);
-
-    const handleMouseUp = () => setIsDragging(false);
-    window.addEventListener("mouseup", handleMouseUp);
-
-    return () => window.removeEventListener("mouseup", handleMouseUp);
-  }, []);
-
-  // debug: selectBlock
-  useEffect(() => {
-    console.log(selectBlock);
-  }, [selectBlock]);
-
-  if (!hydrated) {
-    return null;
-  } else {
-    return (
-      <Card className={className}>
-        <CardContent>
-          <PartyHeader className="mt-5" party={party} />
-          <Separator className="h-1 my-3" />
-          <PartyTimelineHeader
-            className="mt-5"
-            party={party}
-            HandleCheckButton={HandleCheckButton}
-            HandleScheduleButton={HandleScheduleButton}
-            isEditing={isEditing}
-            setIsEditing={setIsEditing}
-            setSelectBlock={setSelectBlock}
-          />
-          {timeLineComponent}
-          {JSON.stringify(votes)}
-        </CardContent>
-      </Card>
-    );
-  }
 };
