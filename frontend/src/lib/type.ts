0bc4f23ad11dc6b0453a4360b5e38001b5d8da57
viiccwen <vic20050418@gmail.com>
2024-08-27 15:59:07 +0800

    chore: add vote-actions return type

--- frontend/src/lib/type.ts
+++ frontend/src/lib/type.ts
@@ -27,6 +27,14 @@ export type general_fetch_return_type = {
   error?: string;
 };
 
+export type userinfo_fetch_return_type = general_fetch_return_type & {
+  data?: {
+    id: number;
+    nickname: string;
+    email: string;
+  };
+};
+
 export type party_data_type = {
   party: {
     title: string;
@@ -38,32 +46,36 @@ export type party_data_type = {
     start_ampm: string;
     end_time: number;
     end_ampm: string;
-  }
-}
-
-export type userinfo_fetch_return_type = general_fetch_return_type & {
-  data?: {
-    id: number;
-    nickname: string;
-    email: string;
   };
 };
 
 export type create_party_fetch_return_type = general_fetch_return_type & {
   data?: {
     partyid: string;
-  }
-}
+  };
+};
 
 export type get_party_fetch_return_type = general_fetch_return_type & {
   data?: {
     party: party_data_type;
-  }
+  };
 };
 
 export type get_partylist_fetch_return_type = general_fetch_return_type & {
   data?: {
     parties: party_data_type[];
-  }
+  };
 };
 
+export type vote_data_type = {
+  creatorName: string;
+  partyid: string;
+  userId: number;
+  timeslots: timeslots_create_schema_type[];
+};
+
+export type get_votetimes_fetch_return_type = general_fetch_return_type & {
+  data?: {
+    vote: vote_data_type;
+  };
+};
