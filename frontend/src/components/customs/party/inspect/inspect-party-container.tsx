50f0b0adfb380e62482ac79749e75aa09b1ae591
viiccwen <vic20050418@gmail.com>
2024-08-27 13:58:58 +0800

    feat: build connection between two client component 1. use zustand to maintain global variable 2. create interactive respondent list when points out specific time block

---
+++ frontend/src/components/customs/party/inspect/inspect-party-container.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { party_return_schema_type, votes_schema_type } from "@/lib/type";
+import { PartyTimelineCard } from "./party-timeline-card";
+import { PartyJoinCard } from "./party-join-card";
+import { create } from "zustand";
+
+interface InspectPartyContainerProps {
+  party: party_return_schema_type;
+  votes: votes_schema_type[];
+  total_hours: number;
+  nickname: string;
+}
+
+type block_type = {
+  creatorName: string;
+};
+
+type position_type = {
+  row: number;
+  col: number;
+};
+
+type party_inspect_type = {
+  all_voteblocks: block_type[][][];
+  cur_points_position: position_type;
+  updateCurPointsPosition: (row: number, col: number) => void;
+  getTimeSlotBlocks: (
+    votes: votes_schema_type[],
+    total_hours: number,
+    party: party_return_schema_type
+  ) => block_type[][][];
+  getUserVoteblocks: (nickname: string) => Set<string>;
+  getJoinList: (votes: votes_schema_type[]) => Set<string>;
+};
+
+export const useVoteBlockStore = create<party_inspect_type>((state) => ({
+  all_voteblocks: [],
+  cur_points_position: { row: -1, col: -1 },
+  updateCurPointsPosition: (row, col) =>
+    state({ cur_points_position: { row, col } }),
+  getTimeSlotBlocks(votes, total_hours, party) {
+    let blocks: Array<block_type[]>[] = Array.from(
+      { length: party.date.length },
+      () => Array.from({ length: total_hours * 2 }, () => [])
+    );
+
+    votes.forEach((vote: votes_schema_type) => {
+      vote.timeslots.forEach((timeslot) => {
+        const date = timeslot.date;
+        const row = party.date.findIndex((v) => v === date);
+
+        const start_time = timeslot.start_time;
+        const end_time = timeslot.end_time;
+        const start_ampm = timeslot.start_ampm;
+        const end_ampm = timeslot.end_ampm;
+
+        const start =
+          (start_time === 12 ? 0 : start_time) +
+          (start_ampm === "PM" ? 12 : 0) -
+          party.start_time;
+        const end =
+          (end_time === 12 ? 0 : end_time) +
+          (end_ampm === "PM" ? 12 : 0) -
+          party.start_time;
+
+        // push start to end blocks
+        for (let i = start; i < end; i += 0.5) {
+          const col = i * 2;
+          blocks[row][col].push({ creatorName: vote.creatorName });
+        }
+      });
+    });
+
+    return blocks;
+  },
+  getUserVoteblocks(nickname) {
+    if (!nickname) return new Set<string>();
+    let userVoteBlocks: Set<string> = new Set();
+
+    this.all_voteblocks.forEach((blocks, row) => {
+      blocks.forEach((block, col) => {
+        block.forEach((vote) => {
+          if (vote.creatorName === nickname) {
+            userVoteBlocks.add(`${row}-${col}`);
+          }
+        });
+      });
+    });
+
+    return userVoteBlocks;
+  },
+  getJoinList(votes: votes_schema_type[]) {
+    return new Set(votes.map((vote) => vote.creatorName));
+  },
+}));
+
+export const InspectPartyContainer = ({
+  party,
+  votes,
+  total_hours,
+  nickname,
+}: InspectPartyContainerProps) => {
+  const vote_blocks = useVoteBlockStore((store) =>
+    store.getTimeSlotBlocks(votes, total_hours, party)
+  );
+  const user_votes = useVoteBlockStore((store) =>
+    store.getUserVoteblocks(nickname)
+  );
+  const join_list = useVoteBlockStore((store) => store.getJoinList(votes));
+
+  return (
+    <>
+      <PartyTimelineCard
+        className="col-span-4"
+        party={party}
+        Allvotes={vote_blocks}
+        userVotes={user_votes}
+        VoteNumber={join_list.size}
+      />
+      <PartyJoinCard className="col-span-2" Allvotes={vote_blocks} joinList={join_list} />
+    </>
+  );
+};
--- frontend/src/components/customs/party/inspect/inspect-party-panel.tsx
+++ frontend/src/components/customs/party/inspect/inspect-party-panel.tsx
@@ -1,39 +1,33 @@
 "use server";
 
-import { party_return_schema_type, votes_schema_type } from "@/lib/type";
-import { PartyJoinCard } from "./party-join-card";
-import { PartyTimelineCard } from "./party-timeline-card";
-import { CreateVote, GetVoteTimes } from "@/actions/vote-actions";
+import { party_return_schema_type } from "@/lib/type";
+import { GetVoteTimes } from "@/actions/vote-actions";
 import { ampm } from "@/lib/schema";
-import { redirect } from "next/navigation";
+import { GetUserInfo } from "@/actions/user-actions";
+import { cookies } from "next/headers";
+import { InspectPartyContainer } from "./inspect-party-container";
 
 interface InspectPartyPanelProps {
   party: party_return_schema_type;
 }
 
-type block_type = {
-  creatorName: string;
-};
-
 export const InspectPartyPanel = async ({ party }: InspectPartyPanelProps) => {
+  const cookie = cookies();
+  const token: string | undefined = cookie.get("token")?.value
+    ? cookie.get("token")?.value
+    : undefined;
+
   const votes = await GetVoteTimes(party.partyid);
+  const userinfo = await GetUserInfo(token);
+  const nickname: string = userinfo.data?.nickname ? userinfo.data.nickname : "";
+
   const total_hours = CalculateTotalHours(party);
-  let voteBlocks: Array<block_type[]>[] = CreateTimeSlotBlocks(
-    votes.data,
-    total_hours,
-    party
-  );
 
   return (
     <>
       <div className="w-full">
         <div className="grid grid-cols-6 gap-6">
-          <PartyTimelineCard
-            className="col-span-4"
-            party={party}
-            votes={voteBlocks}
-          />
-          <PartyJoinCard className="col-span-2" joinList={null} />
+          <InspectPartyContainer party={party} votes={votes.data} total_hours={total_hours} nickname={nickname} />
         </div>
       </div>
     </>
@@ -49,39 +43,4 @@ const CalculateTotalHours = (party: party_return_schema_type): number => {
   end_time = party.end_ampm === ampm[0] ? party.end_time : party.end_time + 12;
 
   return end_time - start_time;
-};
-
-const CreateTimeSlotBlocks = (
-  votes: votes_schema_type[],
-  total_hours: number,
-  party: party_return_schema_type
-) => {
-  let blocks: Array<block_type[]>[] = Array.from(
-    { length: party.date.length },
-    () => Array.from({ length: total_hours * 2 }, () => [])
-  );
-
-  votes.forEach((vote: votes_schema_type) => {
-    vote.timeslots.forEach((timeslot) => {
-      const date = timeslot.date;
-      const row = party.date.findIndex((v) => v === date);
-
-      const start_time = timeslot.start_time;
-      const end_time = timeslot.end_time;
-      const start_ampm = timeslot.start_ampm;
-      const end_ampm = timeslot.end_ampm;
-
-      const start =
-        (start_time === 12 ? 0 : start_time) + (start_ampm === "PM" ? 12 : 0) - party.start_time;
-      const end = (end_time === 12 ? 0 : end_time) + (end_ampm === "PM" ? 12 : 0) - party.start_time;
-      // console.log(start, end);
-      // push start to end blocks
-      for (let i = start; i < end; i += 0.5) {
-        const col = i * 2;
-        blocks[row][col].push({ creatorName: vote.creatorName });
-      }
-    });
-  });
-
-  return blocks;
 };
