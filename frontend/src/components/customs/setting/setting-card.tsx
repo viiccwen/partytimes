229002c8ef76d94c496d4dca0ffe69354c8def74
viiccwen <vic20050418@gmail.com>
2024-08-27 15:36:54 +0800

    feat: change data render file

--- frontend/src/components/customs/setting/setting-card.tsx
+++ frontend/src/components/customs/setting/setting-card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -9,28 +9,20 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Edit2, Trash2, Undo2 } from "lucide-react";
 import { DeleteAccountButton } from "./delete-account-button";
 
-import { Suspense, useEffect, useState } from "react";
 import { HydrateProvider } from "../hydrate-provider";
-import { Skeleton } from "@/components/ui/skeleton";
 import { EditNameButton } from "./edit-name-button";
 import { EditEmailButton } from "./edit-email-button";
 import { BackButton } from "../back-button";
 
-const Cookie = require("js-cookie");
+interface SettingCardProps {
+  id: number;
+  email: string;
+  nickname: string;
+}
 
-export const SettingCard = () => {
-  const [name, setName] = useState<string>(Cookie.get("name") || "");
-  const [email, setEmail] = useState<string>(Cookie.get("email") || "");
-  const [hydrated, setHydrated] = useState<boolean>(false);
-
-  // magic trick
-  useEffect(() => {
-    setHydrated(true);
-  }, []);
-
+export const SettingCard = ({ id, nickname, email }: SettingCardProps) => {
   return (
     <Card>
       <CardHeader>
@@ -50,19 +42,15 @@ export const SettingCard = () => {
             <div className="flex gap-[100px] items-center">
               <p className="font-bold">名稱</p>
               <div className="flex items-center gap-4">
-                <HydrateProvider hydrated={hydrated} width={120} height={20}>
-                  <p>{name}</p>
-                  <EditNameButton value={name} />
-                </HydrateProvider>
+                <p>{nickname}</p>
+                <EditNameButton value={nickname} />
               </div>
             </div>
             <div className="flex gap-[90px] items-center">
               <p className="font-bold">Email</p>
               <div className="flex items-center gap-4">
-                <HydrateProvider hydrated={hydrated} width={120} height={20}>
-                  <p>{email}</p>
-                  <EditEmailButton value={email} />
-                </HydrateProvider>
+                <p>{email}</p>
+                <EditEmailButton value={email} />
               </div>
             </div>
           </div>
@@ -71,7 +59,7 @@ export const SettingCard = () => {
             <Separator />
             <div className="">
               <div className="flex items-center gap-5">
-                <div className="flex flex-col gap-5">
+                <div className="flex flex-col gap-2">
                   <p className="font-bold text-red-500">刪除帳號</p>
                   <p className="text-sm text-slate-500">
                     This will delete your account as well as all events and poll
