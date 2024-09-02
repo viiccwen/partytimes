9430434fb25e7fff3b9cf406b0d9532b2fe38799
viiccwen <vic20050418@gmail.com>
2024-09-02 11:22:43 +0800

    feat: frontend oauth button 1. google 2. github

--- frontend/src/components/customs/user/login-form.tsx
+++ frontend/src/components/customs/user/login-form.tsx
@@ -22,6 +22,8 @@ import { login_schema } from "@/lib/schema";
 import { Button } from "@/components/ui/button";
 import { Login } from "@/actions/user-actions";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { GithubOAuthButton, GoogleOAuthButton } from "./oauth-button";
 
 export const LoginForm = () => {
   const router = useRouter();
@@ -30,14 +32,14 @@ export const LoginForm = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<login_schema_type>({
-    resolver: zodResolver(login_schema)
+    resolver: zodResolver(login_schema),
   });
 
   const onSubmit = async (formdata: login_schema_type) => {
     const response = await Login(formdata);
 
     if (response.correct) {
-        router.push("/profile");
+      router.push("/profile");
     } else {
       toast.error(response.error);
     }
@@ -45,18 +47,20 @@ export const LoginForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Card className={cn("w-[350px] md:w-[400px]")}>
-          <CardHeader>
-            <CardTitle>登入</CardTitle>
-            <CardDescription>登入都不揪？</CardDescription>
-          </CardHeader>
-          <CardContent>
+      <Card className={cn("w-[350px] md:w-[400px]")}>
+        <CardHeader>
+          <CardTitle>登入</CardTitle>
+          <CardDescription>登入都不揪？</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid w-full items-center gap-4 mt-5">
               <div className="flex flex-col gap-8">
                 <Input placeholder="username" {...register("username")} />
                 {errors.username && (
-                    <span className="text-red-500 text-sm">{errors.username.message}</span>
+                  <span className="text-red-500 text-sm">
+                    {errors.username.message}
+                  </span>
                 )}
                 <Input
                   type="password"
@@ -64,19 +68,26 @@ export const LoginForm = () => {
                   {...register("password")}
                 />
                 {errors.password && (
-                    <span className="text-red-500 text-sm">{errors.password.message}</span>
+                  <span className="text-red-500 text-sm">
+                    {errors.password.message}
+                  </span>
                 )}
               </div>
             </div>
-          </CardContent>
-          <CardFooter className="flex justify-between mt-8">
+          </form>
+          <div className="flex justify-between items-center mt-10">
             <Button variant="link" asChild>
               <Link href="/register">還沒有帳號？</Link>
             </Button>
             <Button type="submit">登入</Button>
-          </CardFooter>
-        </Card>
-      </form>
+          </div>
+          <Separator className="mt-5" />
+          <div className="flex flex-col gap-4 mt-5">
+            <GoogleOAuthButton />
+            <GithubOAuthButton />
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
