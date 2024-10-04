9698e431fc493f5f4b0224699c84b089df071963
viiccwen <vic20050418@gmail.com>
2024-10-04 16:23:18 +0800

    fix: fix deploy problem on vercel

--- frontend/src/lib/verify.ts
+++ frontend/src/lib/verify.ts
@@ -1,11 +1,11 @@
+'use server';
 import { Auth } from "@/actions/user-actions";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
-import { cache } from "react";
 
-export const VerifyAuth = cache(async (protected_root: boolean) => {
+export const VerifyAuth = async (protected_root: boolean) => {
   const token = cookies().get("token")?.value;
   const { correct: isAuth, data: user, error } = await Auth(token);
   if (!isAuth && user && protected_root) redirect("/login");
   return { isAuth, user };
-});
+};
