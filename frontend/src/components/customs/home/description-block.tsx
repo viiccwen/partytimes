626ad061a4c6d574c75e9f4d1c3a145764f5dd20
viiccwen <viiccwen@gmail.com>
2024-11-13 15:55:06 +0800

    style: update layout and component styles for improved dark mode support

--- frontend/src/components/customs/home/description-block.tsx
+++ frontend/src/components/customs/home/description-block.tsx
@@ -30,8 +30,8 @@ export const DescriptionBlock = ({
           )}
         >
           <div className="flex flex-col items-center justify-center gap-3 mb-10">
-            <div className="text-sky-200 text-2xl font-bold">{title}</div>
-            <div className="text-slate-100 text-lg">{description}</div>
+            <div className="text-purple-600 dark:text-purple-300 text-2xl font-bold">{title}</div>
+            <div className="text-slate-600 dark:text-slate-300 text-lg">{description}</div>
           </div>
           <div className="relative flex justify-center">
             <div>
