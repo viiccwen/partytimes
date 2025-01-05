3d5004313614071d93c17d554303fd6e0523b6c6
viiccwen <viiccwen@gmail.com>
2025-01-05 15:54:25 +0800

    feat: add delete calendar events
    add eventId in decision schema to keep calendar event id
    add delete google-calendar event when user cancels the event on partytimes.

--- backend/controllers/calendar-controllers.ts
+++ backend/controllers/calendar-controllers.ts
@@ -40,7 +40,8 @@ export const getCalendarEvents = async (req: any, res: any) => {
 
 export const createCalendarEvent = async (req: any, res: any) => {
   try {
-    const { refresh_token, summary, description, start, end, invited_lists } = await req.body;
+    const { refresh_token, summary, description, start, end, invited_lists, partyid } =
+      await req.body;
 
     if (!summary || !start || !end) throw new Error("請填寫所有欄位！");
 
@@ -77,7 +78,14 @@ export const createCalendarEvent = async (req: any, res: any) => {
       requestBody: event,
     });
 
-    console.log(response.data);
+    if (!response.data.id) throw new Error("新增日曆失敗！");
+
+    const update_eventId = await prisma.decision.update({
+      where: { partyid },
+      data: { eventId: response.data.id },
+    });
+
+    if (!update_eventId) throw new Error("更新日曆失敗！");
 
     res.status(200).json({ message: "新增成功！" });
   } catch (error: any) {
@@ -88,29 +96,25 @@ export const createCalendarEvent = async (req: any, res: any) => {
 export const deleteCalendarEvent = async (req: any, res: any) => {
   try {
     const user = await req.user;
-    const { eventId } = await req.params;
+    const { eventId, refresh_token } = await req.body;
 
     if (!eventId) {
       throw new Error("請填寫活動編號！");
     }
 
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.AUTH_GOOGLE_ID,
-      process.env.AUTH_GOOGLE_SECRET,
-      `${process.env.AUTH_REDIRECT_URL}/api/auth/callback/google`
-    );
-
-    oauth2Client.setCredentials({
-      access_token: user.accessToken,
-      refresh_token: user.refreshToken,
-    });
+    // refresh token
+    const oauth2Client = await refreshAccessToken(refresh_token);
 
     const calendar = google.calendar({ version: "v3", auth: oauth2Client });
 
-    await calendar.events.delete({
+    const del_event = await calendar.events.delete({
       calendarId: "primary",
       eventId,
     });
+
+    if (!del_event) {
+      throw new Error("刪除活動失敗！");
+    }
 
     res.status(200).json({ message: "刪除成功！" });
   } catch (error: any) {
